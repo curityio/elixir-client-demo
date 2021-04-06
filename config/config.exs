eecd00774bce529d008345c53ed7df91cbc32c04
iggbom <jonas@curity.io>
2021-04-06 14:45:27 -0700

    Updated config file and added gitignore

--- config/config.exs
+++ config/config.exs
@@ -4,7 +4,7 @@ config :logger, level: :debug
 
 config :curity_app,
     client_id: "elixir",
-    client_secret: "Password1",
+    client_secret: "Pa$$w0rd!",
     site: "http://localhost:8443",
     token_url: "http://localhost:8443/oauth/v2/oauth-token",
     scope: ""
